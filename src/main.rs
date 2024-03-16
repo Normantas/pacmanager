3bfbf2f9292acdd17b6e48f939fc6d086847a939
randomguy <randomguy@somewhere.nowhere>
2024-03-16 15:57:51 +0200

    Fix bugs, add package manager support

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use dialoguer::Input;
 use futures_lite::{io::BufReader, prelude::*};
 
-use pacmanager_wrapper::{execute_action, PacManagerAction, PacManagerCommand};
+use pacmanager_wrapper::{execute_action, PacManager, PacManagerAction, PacManagerCommand};
 
 #[derive(clap::ValueEnum, Clone, Debug)]
 pub enum Command {
@@ -29,12 +29,12 @@ struct Args {
     package: String,
 }
 
-fn command_to_pacmanager_command(command: Command, mut package: String) -> PacManagerCommand {
+fn command_to_pacmanager_command(command: Command, mut package: String) -> (PacManagerCommand, bool) {
     // Commands which do not require a package
     match command {
-        Command::List => return PacManagerCommand::List,
-        Command::Update => return PacManagerCommand::Update,
-        Command::Upgrade => return PacManagerCommand::Update,
+        Command::List => return (PacManagerCommand::List, false),
+        Command::Update => return (PacManagerCommand::Update, false),
+        Command::Upgrade => return (PacManagerCommand::Update, false),
         _ => (),
     };
 
@@ -44,28 +44,41 @@ fn command_to_pacmanager_command(command: Command, mut package: String) -> PacMa
     };
 
     match command {
-        Command::Install => PacManagerCommand::Install(package),
-        Command::Reinstall => PacManagerCommand::Reinstall(package),
-        Command::Remove => PacManagerCommand::Uninstall(package),
-        Command::View => PacManagerCommand::View(package),
-        Command::Search => PacManagerCommand::Search(package),
+        Command::Install => (PacManagerCommand::Install(package), true),
+        Command::Reinstall => (PacManagerCommand::Reinstall(package), true),
+        Command::Remove => (PacManagerCommand::Uninstall(package), true),
+        Command::View => (PacManagerCommand::View(package), false),
+        Command::Search => (PacManagerCommand::Search(package), false),
         _ => unreachable!(),
+    }
+}
+
+fn get_package_manager() -> PacManager {
+    let identify = whatadistro::identify().unwrap();
+    if identify.is_similar("arch") {
+        PacManager::Pacman
+    } else if identify.is_similar("debian") {
+        PacManager::Apt
+    } else if identify.is_similar("rhel") || identify.is_similar("fedora") {
+        PacManager::Yum
+    } else {
+        panic!("Unsupported package manager")
     }
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let pacmanager_command = command_to_pacmanager_command(args.command, args.package);
+    let (pacmanager_command, should_be_noninteractive) = command_to_pacmanager_command(args.command, args.package);
 
     let action = PacManagerAction {
         pacmanager_command,
         internal_config: Default::default(),
-        non_interactive: true,
+        non_interactive: should_be_noninteractive,
         custom_flags: None,
     };
 
-    let mut child = execute_action(action, pacmanager_wrapper::PacManager::Apt)
+    let mut child = execute_action(action, get_package_manager())
         .await
         .unwrap();
     let mut lines = BufReader::new(child.stdout.take().unwrap()).lines();
